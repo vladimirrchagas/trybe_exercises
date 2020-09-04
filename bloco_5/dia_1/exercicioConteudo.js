d321ed52239aed313827cc70b97c9fb3742b3e0a
Vladimir <vladimirrchagas@gmail.com>
2020-09-04 17:44:34 -0300

    Exercicio do conteúdo

---
+++ bloco_5/dia_1/exercicioConteudo.js
@@ -0,0 +1,14 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+paragrafo1 = document.getElementsByClassName("paragrafo1");
+paragrafo1[0].style.color = "darkgreen";
+
+document.getElementById("page-title").innerText = "Vai Curinthia";
+
+document.getElementById("second-paragraph").innerText = "Corinthians, o melhor time do mundo! \n Apresentando a maior e mais fanática torcida que todos já viram! Vai Curinthiaaaaa! \n Aqui tem um bando de louco, \n louco por ti curinthia!!!!";
+
+document.getElementById("subtitle").innerText = "Corinthians, o campeão dos campeões!";
+
+subtitulo = document.getElementsByTagName("h4");
+console.log(subtitulo[0].innerText);
