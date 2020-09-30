f7ff9296c34ff8576b88e328a235c72328ce076d
vladimirrchagas <vladimirrchagas@gmail.com>
2020-09-30 14:31:46 -0300

    Exercício 2.1 / 2.2 / 2.3 / 2.4 - Dia 3 - Bloco 7

--- bloco_7/dia_3/exercicio1.2.js
+++ bloco_7/dia_3/exercicio1.2.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-function myRemove(arr, item) {
+const myRemove = (arr, item) => {
   let newArr = [];
   for (let i = 0; i < arr.length; i += 1) {
     if (item !== arr[i]) {
