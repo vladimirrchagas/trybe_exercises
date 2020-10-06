102f90b0dec868ab4d9f94769167733ef49f1894
vladimirrchagas <vladimirrchagas@gmail.com>
2020-10-06 15:23:20 -0300

    Add no repo trybe-exercises o projeto unit tests (7.4)

---
+++ bloco_7/dia_4/src/vqv.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+
+/*
+  Use template literals para escrever uma função que,
+  recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
+  Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
+
+  Parâmetros:
+    - Uma string;
+    - Um número.
+  Comportamento:
+    vqv(Tunico, 30) // Retorna:
+      'Oi, meu nome é Tunico!
+      Tenho 30 anos,
+      trabalho na Trybe e mando muito em programação!
+      #VQV!'
+*/
+
+const vqv = (nome, idade) => {
+  if (nome == null || idade == null) {
+    return undefined;
+  }
+  const me = {
+    name: nome,
+    age: idade,
+  };
+  return (`Oi, meu nome é ${me.name}!\n` +
+  `Tenho ${me.age} anos,\n` +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!');
+};
+
+module.exports = vqv;
