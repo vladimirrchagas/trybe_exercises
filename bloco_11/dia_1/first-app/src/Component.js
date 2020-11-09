563ff633c2548400626b20014355d45cd2555c8a
vladimirrchagas <vladimirrchagas@gmail.com>
2020-11-09 11:30:17 -0300

    Exercicios 1 e 2 Dia 1 - Bloco 11

---
+++ bloco_11/dia_1/first-app/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FirstApp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Vladimir Rambaldi Chagas</h1>
+        <p>Estudante Full Stack Tryber! Ansioso para Iniciar no mercado!</p>
+      </div>
+    );
+  }
+}
+
+export default FirstApp;
