bccf68b7866e1a49741b3ca104c1cd75535c7611
vladimirrchagas <vladimirrchagas@gmail.com>
2021-01-19 11:22:40 -0300

    block 16 day 5

---
+++ bloco_16/dia_5/counter/src/helpers/renderWithRedux.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { render, cleanup } from '@testing-library/react';
+import { createStore, combineReducers } from 'redux';
+import clickReducer from '../reducers';
+
+export const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
